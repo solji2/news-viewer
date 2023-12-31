dc15f944e3e8446abac4078ab54d2c9f7d3d0c78
solji2 <thfwl0094@naver.com>
2023-12-31 23:56:47 +0900

    20231231

--- src/App.js
+++ src/App.js
@@ -2,31 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import axios from "axios";
+import NewsItem from "./components/NewsItem";
 
-function App() {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get("http://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => {
-        setData(response.data);
-      });
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-        {data && (
-          <textarea
-            rows={7}
-            value={JSON.stringify(data, null, 2)}
-            readOnly={true}
-          ></textarea>
-        )}
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <NewsItem />;
+};
 
 export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+const NewsItemBlock = styled.div`
+  display: flex;
+  .thumnail {
+    margin-right: 1rem;
+    img {
+      display: block;
+      width: 160;
+      height: 100;
+      object-fit: cover;
+    }
+  }
+  .contents {
+    h2 {
+      margin: 0;
+      a {
+        color: black;
+      }
+    }
+    p {
+      margin: 0;
+      line-height: 1.5;
+      margin-top: 0.5rem;
+      white-space: normal;
+    }
+  }
+  & + & {
+    margin-top: 3rem;
+  }
+`;
+
+const NewsItem = (article) => {
+  //title:제목
+  //description:내용
+  //url:링크
+  //urlToImage:뉴스 이미지
+  const { title, description, url, urlToImage } = article;
+
+  return (
+    <NewsItemBlock>
+      {!urlToImage && (
+        <div className="thumnail">
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <img src={urlToImage} alt="thumnail" />
+          </a>
+        </div>
+      )}
+      <div className="contents">
+        <h2>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </NewsItemBlock>
+  );
+};
+export default NewsItem;
