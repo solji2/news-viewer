aa0831cbf583f36df49558e23898458672556220
solji2 <thfwl0094@naver.com>
2024-01-05 00:13:52 +0900

    20240105

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { useState } from "react";
 import axios from "axios";
 import NewsItem from "./components/NewsItem";
 import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 const App = () => {
-  return <NewsList />;
+  return (
+    <>
+      <Categories />
+      <NewsList />;
+    </>
+  );
 };
 
 export default App;
